d3142ae3c12816386db510ab64ddca3dd41650b5
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-01-02 02:18:55 +0000

    Initial Commit

--- src/software/ulpgc/kata2/HistogramCalculator.java
+++
@@ -1,7 +0,0 @@
-package software.ulpgc.kata2;
-
-import java.util.Map;
-
-public interface HistogramCalculator {
-    Map<String, Integer> calculate();
-}
--- src/software/ulpgc/kata2/PersonLoader.java
+++
@@ -1,7 +0,0 @@
-package software.ulpgc.kata2;
-
-import java.util.List;
-
-public interface PersonLoader {
-    List<Person> load();
-}
