cfc121f6d6e04e8a632c1ee12caf99223941aeed
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-01-02 00:51:37 +0000

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
-import software.ulpgc.kata2.BMIHistogramCalculator;
-import software.ulpgc.kata2.CsvFilePersonLoader;
-import software.ulpgc.kata2.Person;
-
-import java.util.List;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        List<Person> people = CsvFilePersonLoader.with("bmi_dataset.csv").load();
-        Map<String, Integer> histogram = new BMIHistogramCalculator(people).calculate();
-        for (String key: histogram.keySet().stream().sorted().toList()){
-            System.out.println(key + " " + histogram.get(key));
-        }
-
-
+        System.out.println("Hello world!");
     }
 }
